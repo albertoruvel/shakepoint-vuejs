bd0bb985a4758daf6808501222ec289bbb333571
= <=>
2018-07-19 19:25:04 -0700

    Updated packages

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,4 +1,4 @@
-let host = 'http://35.160.133.118/shakepoint-rest-api/rest/v1/'
+let host = 'https://35.160.133.118/shakepoint-rest-api/rest/v1/'
 //let host = 'http://localhost:8080/shakepoint-rest-api/rest/v1/'
 export function getAddress(path) {
     return host + path
